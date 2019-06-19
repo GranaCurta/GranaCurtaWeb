b26993f7e0a3a81e56bf09d6fcc5bde1027b60ee
Cristiano Serafim Lauffer <cristiano.serafim.lauffer@gmail.com>
2019-06-18 21:51:51 -0300

    GranaCurta/GranaCurtaWeb#26 - US-009 - Manter movimentações de contas/cartões - Versionamento parcial da funcionalidade - Somente com a consulta de movimentações

--- GranaCurtaDAL/DAL/MovimentacoesDAL.cs
+++ GranaCurtaDAL/DAL/MovimentacoesDAL.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace GranaCurtaDAL.DAL
 {
@@ -85,6 +83,184 @@ namespace GranaCurtaDAL.DAL
             }
 
             return dtbRetorno;
+        }
+
+        public int insert(string strData, string strDataReferencia, string strDescricao, int? intNumeroParcela, int? intQtdParcelas, double dblValor, int intIdUsuario, int intIdCategoria, int? intIdCartao, int? intIdConta)
+        {
+            int intRetorno = -1;
+            StringBuilder strQuery = new StringBuilder();
+
+            try
+            {
+                strQuery.AppendLine("INSERT INTO granacurta.tb_movimentacoes");
+                strQuery.AppendLine("(dt_movimentacao, dt_referencia, ds_movimentacao, nu_parcela, qt_parcelas, vl_movimentacao, id_usuario, id_categoria, id_cartao, id_conta, dt_criacao) VALUES");
+                strQuery.AppendLine("(@dt_movimentacao, @dt_referencia, @ds_movimentacao, @nu_parcela, @qt_parcelas, @vl_movimentacao, @id_usuario, @id_categoria, @id_cartao, @id_conta, GETDATE())");
+                strQuery.AppendLine("SELECT CAST(scope_identity() AS int) id;");
+
+                using (SqlConnection connection = new SqlConnection(this.strConnStr))
+                {
+                    SqlCommand command = new SqlCommand(strQuery.ToString(), connection);
+
+                    command.Parameters.AddWithValue("@dt_movimentacao", strData);
+                    command.Parameters.AddWithValue("@dt_referencia", strDataReferencia);
+                    command.Parameters.AddWithValue("@ds_movimentacao", strDescricao);
+                    command.Parameters.AddWithValue("@nu_parcela", intNumeroParcela);
+                    command.Parameters.AddWithValue("@qt_parcelas", intQtdParcelas);
+                    command.Parameters.AddWithValue("@vl_movimentacao", dblValor);
+                    command.Parameters.AddWithValue("@id_usuario", intIdUsuario);
+                    command.Parameters.AddWithValue("@id_categoria", intIdCategoria);
+                    command.Parameters.AddWithValue("@id_cartao", intIdCartao);
+                    command.Parameters.AddWithValue("@id_conta", intIdConta);
+
+                    connection.Open();
+
+                    intRetorno = (int)command.ExecuteScalar();
+
+                    connection.Close();
+
+                }
+            }
+            catch (Exception)
+            {
+                intRetorno = -1;
+            }
+
+            return intRetorno;
+        }
+
+        public int update(int intIdMovimentacao, string strData, string strDataReferencia, string strDescricao, int? intNumeroParcela, int? intQtdParcelas, double dblValor, int intIdUsuario, int intIdCategoria, int? intIdCartao, int? intIdConta)
+        {
+            int intRetorno = -1;
+            StringBuilder strQuery = new StringBuilder();
+
+            try
+            {
+                strQuery.AppendLine("UPDATE M SET");
+                strQuery.AppendLine("	dt_movimentacao = @dt_movimentacao,");
+                strQuery.AppendLine("	dt_referencia = @dt_referencia,");
+                strQuery.AppendLine("	ds_movimentacao = @ds_movimentacao,");
+                strQuery.AppendLine("	nu_parcela = @nu_parcela,");
+                strQuery.AppendLine("	qt_parcelas = @qt_parcelas,");
+                strQuery.AppendLine("	vl_movimentacao = @vl_movimentacao,");
+                strQuery.AppendLine("	id_categoria = @id_categoria,");
+                strQuery.AppendLine("	id_cartao = @id_cartao,");
+                strQuery.AppendLine("	id_conta = @id_conta,");
+                strQuery.AppendLine("	dt_alteracao = GETDATE()");
+                strQuery.AppendLine("FROM granacurta.tb_movimentacoes M");
+                strQuery.AppendLine("WHERE");
+                strQuery.AppendLine("	M.id_movimentacao = @id_movimentacao");
+                strQuery.AppendLine("	AND M.id_usuario = @id_usuario");
+
+                using (SqlConnection connection = new SqlConnection(this.strConnStr))
+                {
+                    SqlCommand command = new SqlCommand(strQuery.ToString(), connection);
+
+                    command.Parameters.AddWithValue("@dt_movimentacao", strData);
+                    command.Parameters.AddWithValue("@dt_referencia", strDataReferencia);
+                    command.Parameters.AddWithValue("@ds_movimentacao", strDescricao);
+                    command.Parameters.AddWithValue("@nu_parcela", intNumeroParcela);
+                    command.Parameters.AddWithValue("@qt_parcelas", intQtdParcelas);
+                    command.Parameters.AddWithValue("@vl_movimentacao", dblValor);
+                    command.Parameters.AddWithValue("@id_categoria", intIdCategoria);
+                    command.Parameters.AddWithValue("@id_cartao", intIdCartao);
+                    command.Parameters.AddWithValue("@id_conta", intIdConta);
+                    command.Parameters.AddWithValue("@id_movimentacao", intIdMovimentacao);
+                    command.Parameters.AddWithValue("@id_usuario", intIdUsuario);
+
+                    connection.Open();
+
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        intRetorno = intIdMovimentacao;
+                    }
+
+                    connection.Close();
+
+                }
+            }
+            catch (Exception)
+            {
+                intRetorno = -1;
+            }
+
+            return intRetorno;
+        }
+
+        public int delete(int intIdUsuario, int intIdMovimentacao)
+        {
+            int intAffecRows = -1;
+            StringBuilder strQuery = new StringBuilder();
+
+            try
+            {
+                strQuery.AppendLine("DELETE FROM granacurta.tb_movimentacoes");
+                strQuery.AppendLine("WHERE ");
+                strQuery.AppendLine("   id_movimentacao = @id_movimentacao");
+                strQuery.AppendLine("   AND id_usuario = @id_usuario");
+
+                using (SqlConnection connection = new SqlConnection(this.strConnStr))
+                {
+                    SqlCommand command = new SqlCommand(strQuery.ToString(), connection);
+
+                    command.Parameters.AddWithValue("@id_usuario", intIdUsuario);
+                    command.Parameters.AddWithValue("@id_movimentacao", intIdMovimentacao);
+
+                    connection.Open();
+
+                    intAffecRows = command.ExecuteNonQuery();
+
+                    connection.Close();
+
+                }
+            }
+            catch (Exception)
+            {
+                intAffecRows = -1;
+            }
+
+            return intAffecRows;
+        }
+
+        public int delete(int intIdUsuario, List<int> lstMovimentacoes)
+        {
+            int intAffecRows = -1;
+            StringBuilder strQuery = new StringBuilder();
+            StringBuilder strIdMovimentacoes = new StringBuilder();
+
+            try
+            {
+                foreach (int item in lstMovimentacoes)
+                {
+                    strIdMovimentacoes.Append(item.ToString() + ",");
+                }
+
+                strQuery.AppendLine("DELETE FROM granacurta.tb_movimentacoes");
+                strQuery.AppendLine("WHERE ");
+                strQuery.AppendLine("   id_movimentacao IN( @id_movimentacoes )");
+                strQuery.AppendLine("   AND id_usuario = @id_usuario");
+
+                using (SqlConnection connection = new SqlConnection(this.strConnStr))
+                {
+                    SqlCommand command = new SqlCommand(strQuery.ToString(), connection);
+
+                    command.Parameters.AddWithValue("@id_usuario", intIdUsuario);
+                    //command.Parameters.AddWithValue("@id_movimentacao", intIdMovimentacao);
+                    command.Parameters.AddWithValue("@id_movimentacoes", strIdMovimentacoes.ToString().TrimEnd(','));
+
+                    connection.Open();
+
+                    //intAffecRows = command.ExecuteNonQuery();
+
+                    connection.Close();
+
+                }
+            }
+            catch (Exception)
+            {
+                intAffecRows = -1;
+            }
+
+            return intAffecRows;
         }
 
         private StringBuilder GetQuerySelect()
